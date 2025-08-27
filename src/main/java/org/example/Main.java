5580330ed58f13dca3044d070a55ffad5c1e06f2
NeiNeiP1 <jramosb@unsa.edu.pe>
2025-08-27 15:20:29 -0500

    Calculadora 2 number

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,52 @@
 package org.example;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sca=new Scanner(System.in);
+        Integer opcion;
+        Integer num1;
+        Integer num2;
+        Integer resultado=0;
+        do{
+            resultado=0;
+            System.out.println("------------Calculadora------------C");
+            System.out.println("1. SUmar");
+            System.out.println("2. Restar");
+            System.out.println("3. Multiplicar");
+            System.out.println("4. Dividir");
+            System.out.println("5. Cerrar");
+            System.out.print("Selecciona una opción: ");
+            opcion=sca.nextInt();
+            if(opcion!=5){
+                System.out.print("Ingrese el primer nýmero: ");
+                num1=sca.nextInt();
+                System.out.print("Ingrese el segundo nýmero: ");
+                num2=sca.nextInt();
+                switch(opcion){
+                    case 1:
+                        resultado = num1+num2;
+                        break;
+                    case 2:
+                        resultado = num1-num2;
+                        break;
+                    case 3:
+                        resultado = num1*num2;
+                        break;
+                    case 4:
+                        if(num2!=0)
+                            resultado = num1/num2;
+                        else{
+                            System.out.println("No se puede dividir entre 0");
+                        }
+                        break;
+                }
+                System.out.println("Resuiltado: "+resultado);
 
+            }
+        }while(opcion!=5);
     }
 }
